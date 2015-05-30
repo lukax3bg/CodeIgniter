746546ad17901a191c7aab38f0a3df264a5c0d2d
lukax3bg <lk.jovanovic93@gmail.com>
2015-05-30 04:15:02 +0200

    sign up

---
+++ application/views/home/homepage.php
@@ -0,0 +1,76 @@
+<html>
+
+<head>
+	
+        <link rel="stylesheet" type="text/css" href="<?php echo base_url()."/assets/css/css_index/style.css"; ?>"> 
+		 <link rel="stylesheet" type="text/css" href="css/style.css" />
+</head>
+
+
+
+
+<body>
+<div id="bg">
+	<img src="<?php echo base_url()."/assets/images/psi.jpg"; ?>" width="100%" height="100%" style="position: fixed; top: 0px; left: 0px;">
+</div>
+<div id="container">
+	
+			
+			
+			<section class="main">
+							
+				<div class="logovanje">
+							
+							
+				    <h1>Login</h1>
+				    <p>
+				        <label for="login">Username</label>
+				        <input type="text" placeholder="username" name="username" >
+				    </p>
+				    <p>
+				        <label for="password">Password</label>
+				        <input type="password" placeholder="password" name="password"> 
+				    </p>
+
+				    <p>
+				        <input type="submit" name="submit" value="sign in">
+				    </p>       
+				</div>
+			</section>
+			
+			<!-- druga -->
+			
+			<section class="main">
+							
+				<div class="logovanje">
+							
+							
+				    <h1>Register</h1>
+				    <p>
+				        <label for="login">Username</label>
+				        <input type="text" placeholder="username" name="username" >
+				    </p>
+					 <p>
+				        <label for="password">Password</label>
+				        <input type="password" placeholder="password" name="password"> 
+				    </p>
+					<p>
+				        <label for="login">Repeat password</label>
+				        <input type="text" placeholder="re-password" name="re-password" >
+				    </p>
+					<p>
+				        <label for="login">E-mail</label>
+				        <input type="text" placeholder="email" name="email" >
+				    </p>
+				   
+
+				    <p>
+				        <input type="submit" name="submit" value="sign up">
+				    </p>       
+				</div>
+			</section>
+			
+			
+ </div>
+ </body>
+ </html>
