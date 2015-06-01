f3a43c9418c9908a9ab856ffa0338edd71629704
lukax3bg <lk.jovanovic93@gmail.com>
2015-06-01 23:26:25 +0200

    pogledaj

--- application/views/board/toolbar.php
+++ application/views/board/toolbar.php
@@ -10,7 +10,7 @@
 				
 				<div class="nav-icon">
 				<p>Pin<a href="<?php echo base_url()."index.php/BoardController"; ?>">Board</a></p>
-					<?php echo form_open('UserController/logout');  ?> <input type="submit"  name="submit" value=""  class="right_bt" id="activator" /><span> </span> </a></form>
+					
 				</div>
 				        	  
 				<div class="top-searchbar">
@@ -19,9 +19,10 @@
 					</form>
 				</div>
 				<div class="userinfo">
+				
 					<div class="user">
 						<ul>
-
+							<?php echo form_open('UserController/logout');  ?> <input type="submit"  name="submit" value=""  class="right_bt" id="activator" /><span> </span> </a></form>
 							<li><a href="<?php echo base_url()."index.php/BoardController/editUser"; ?>"><img src="<?php echo base_url()."/assets/images/user-pic.png"; ?>" title="user-name" /><span><?php echo $ime; ?></span></a></li>
 
 							
